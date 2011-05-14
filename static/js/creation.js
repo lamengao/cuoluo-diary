b0b1f70d11f512ba24d1aef99184d39bad4bf84c
Lamengao <lamengao@gmail.com>
2011-05-15 04:09:59 +0800

    create new and today button display done

--- static/js/creation.js
+++ static/js/creation.js
@@ -8,9 +8,15 @@ goog.provide('cld.Creation');
 
 goog.require('goog.events');
 goog.require('goog.events.EventTarget');
-goog.require('goog.ui.CustomButton');
-//goog.require('goog.ui.CustomButtonRenderer');
 goog.require('goog.ui.Css3ButtonRenderer');
+goog.require('goog.ui.Css3MenuButtonRenderer');
+goog.require('goog.ui.CustomButton');
+goog.require('goog.ui.ImagelessMenuButtonRenderer');
+goog.require('goog.ui.Menu');
+goog.require('goog.ui.MenuButton');
+goog.require('goog.ui.MenuItem');
+goog.require('goog.ui.Separator');
+goog.require('goog.userAgent');
 
 /**
  * Create new.
@@ -26,10 +32,113 @@ cld.Creation = function(app, opt_domHelper) {
   this.dom_ = opt_domHelper || goog.dom.getDomHelper();
 
   this.elContainer = this.dom_.getElement('createnew');
-  
-  this.button = new goog.ui.CustomButton(null,
-    goog.ui.Css3ButtonRenderer.getInstance());
-  this.button.decorate(this.elContainer);
-  this.button.setCaption('Create new');
+
+  this.createMenu_();
+  this.childnoteMenuItem.setEnabled(false);
+  this.siblingnoteMenuItem.setEnabled(false);
+
+  this.createMenuButton_();
 };
 goog.inherits(cld.Creation, goog.events.EventTarget);
+
+/**
+ * Create 'create new' menu.
+ * @private
+ */
+cld.Creation.prototype.createMenu_ = function() {
+  this.menu = new goog.ui.Menu();
+  var makeItemContent = function(caption) {
+    var htmlStr = '<div class="goog-menuitem-icon ' + caption +
+                  '-menuitemicon"></div>' + caption;
+    return goog.dom.htmlToDocumentFragment(htmlStr);
+  };
+  var items = [
+    this.diaryMenuItem = new goog.ui.MenuItem(makeItemContent('Diary')),
+    this.noteMenuItem = new goog.ui.MenuItem(makeItemContent('Note')),
+    this.emailMenuItem = new goog.ui.MenuItem(makeItemContent('Email')),
+    new goog.ui.MenuSeparator(),
+    this.childnoteMenuItem =
+      new goog.ui.MenuItem(makeItemContent('Child note')),
+    this.siblingnoteMenuItem =
+      new goog.ui.MenuItem(makeItemContent('Sibling note'))
+  ];
+  this.diaryMenuItem.setId('diarymenuitem');
+  this.noteMenuItem.setId('notemenuitem');
+  this.emailMenuItem.setId('emailmenuitem');
+  this.childnoteMenuItem.setId('childnotemenuitem');
+  this.siblingnoteMenuItem.setId('siblingnotemenuitem');
+  goog.array.forEach(items, function(item) {
+      this.menu.addItem(item);
+  }, this);
+  var el = this.menu.getElement();
+  el.id = 'createnewmenu';
+
+  goog.events.listen(this.menu, 'action', function(e) {
+      var id = e.target.getId();
+      if (id === 'diarymenuitem') {
+        this.createNewDiary();
+      } else if (id == 'notemenuitem') {
+        this.createNewNote();
+      } else if (id == 'emailmenuitem') {
+        this.createNewEmail();
+      } else if (id == 'childnotemenuitem') {
+        this.createNewChildNote();
+      } else if (id == 'siblingnotemenuitem') {
+        this.createNewSiblingNote();
+      }
+  }, false, this);
+};
+
+/**
+ * create menu button.
+ * @private
+ */
+cld.Creation.prototype.createMenuButton_ = function() {
+  this.menuButton = new goog.ui.MenuButton('Create new', this.menu,
+    cld.Creation.getRightMenuButtonRenderer());
+  this.menuButton.render(this.dom_.getElement('createnew'));
+};
+
+/**
+ * Get right button renderer.
+ * If css3 support return Css3ButtonRenderer, else return CustomButtonRenderer.
+ * @return {goog.ui.ButtonRenderer} renderer.
+ */
+cld.Creation.getRightMenuButtonRenderer = function() {
+  if (cld.ui.canUseCSS3Button()) {
+    return goog.ui.Css3MenuButtonRenderer.getInstance();
+  } else {
+    return goog.ui.ImagelessMenuButtonRenderer.getInstance();
+  }
+};
+
+/**
+ * Create new diary.
+ */
+cld.Creation.prototype.createNewDiary = function() {
+  alert('new diary');
+};
+/**
+ * Create new Note.
+ */
+cld.Creation.prototype.createNewNote = function() {
+  alert('new note');
+};
+/**
+ * Create new Email.
+ */
+cld.Creation.prototype.createNewEmail = function() {
+  alert('new email');
+};
+/**
+ * Create new child note.
+ */
+cld.Creation.prototype.createNewChildNote = function() {
+  alert('new child note');
+};
+/**
+ * Create new sibling note.
+ */
+cld.Creation.prototype.createNewSiblingNote = function() {
+  alert('new sibling note');
+};
--- static/js/today.js
+++ static/js/today.js
@@ -8,9 +8,10 @@ goog.provide('cld.Today');
 
 goog.require('goog.events');
 goog.require('goog.events.EventTarget');
-goog.require('goog.ui.CustomButton');
-//goog.require('goog.ui.CustomButtonRenderer');
 goog.require('goog.ui.Css3ButtonRenderer');
+goog.require('goog.ui.CustomButton');
+goog.require('goog.ui.ImagelessButtonRenderer');
+goog.require('goog.userAgent');
 
 /**
  * Today button.
@@ -26,10 +27,34 @@ cld.Today = function(app, opt_domHelper) {
   this.dom_ = opt_domHelper || goog.dom.getDomHelper();
 
   this.elContainer = this.dom_.getElement('today');
-  
+
   this.button = new goog.ui.CustomButton(null,
-    goog.ui.Css3ButtonRenderer.getInstance());
+    cld.Today.getRightButtonRenderer());
   this.button.decorate(this.elContainer);
   this.button.setCaption('Today');
+
+  goog.events.listen(this.button, goog.ui.Component.EventType.ACTION,
+    goog.bind(this.goToday, this));
 };
 goog.inherits(cld.Today, goog.events.EventTarget);
+
+/**
+ * Go to the diary of today.
+ * @param {goog.events.Event}} e toggle event.
+ */
+cld.Today.prototype.goToday = function(e) {
+  alert('today');
+};
+
+/**
+ * Get right button renderer.
+ * If css3 support return Css3ButtonRenderer, else return CustomButtonRenderer.
+ * @return {goog.ui.ButtonRenderer} renderer.
+ */
+cld.Today.getRightButtonRenderer = function() {
+  if (cld.ui.canUseCSS3Button()) {
+    return goog.ui.Css3ButtonRenderer.getInstance();
+  } else {
+    return goog.ui.ImagelessButtonRenderer.getInstance();
+  }
+};
