bbab29d1a606de129630ca0b6dd304e40f55b1cb
Lamengao <lamengao@gmail.com>
2014-04-25 14:44:17 +0800

    app.yaml add libraries

--- appengine_config.py
+++ appengine_config.py
@@ -1,19 +1,19 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-import os 
-import sys 
+#import os
+#import sys
 
-os.environ['DJANGO_SETTINGS_MODULE'] = 'djangosettings' 
-for k in [k for k in sys.modules if k.startswith('django')]: 
-	del sys.modules[k] 
+#os.environ['DJANGO_SETTINGS_MODULE'] = 'djangosettings'
+#for k in [k for k in sys.modules if k.startswith('django')]:
+    #del sys.modules[k]
 
-from google.appengine.dist import use_library 
-use_library('django', '1.2')
+#from google.appengine.dist import use_library
+#use_library('django', '1.2')
 
 
 def namespace_manager_default_namespace_for_request():
-	return 'newyear'
+    return 'newyear'
 
 
 remoteapi_CUSTOM_ENVIRONMENT_AUTHENTICATION = (
-		'HTTP_X_APPENGINE_INBOUND_APPID', ['cuoluodiary', 'cuoluo-diary'])
+    'HTTP_X_APPENGINE_INBOUND_APPID', ['cuoluodiary', 'cuoluo-diary'])
