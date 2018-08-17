7bf91f9a90914be27fc31ed32c20fc329d1d3689
Lamengao <lamengao@gmail.com>
2018-08-18 01:08:51 +0800

    add donate link in app

--- tools/rmpyc.sh
+++ tools/rmpyc.sh
@@ -2,9 +2,3 @@
 
 find /Users/yibing/developer/projects/GAE/cldiary/newyear -name "*.pyc"
 find /Users/yibing/developer/projects/GAE/cldiary/newyear -name "*.pyc"|xargs rm
-<<<<<<< HEAD
-
-#hahahah hahahah
-#test deployment script
-=======
->>>>>>> develop
